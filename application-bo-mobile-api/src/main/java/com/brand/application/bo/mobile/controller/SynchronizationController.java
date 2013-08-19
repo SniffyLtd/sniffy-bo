f56a6268f6149cbace68ed5a778a175eae1d3a78
unknown <Tomek@Tomek-Komputer.(none)>
2013-08-19 20:18:29 +0200

    RF# dev

---
+++ application-bo-mobile-api/src/main/java/com/brand/application/bo/mobile/controller/SynchronizationController.java
@@ -0,0 +1,24 @@
+package com.brand.application.bo.mobile.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/sync")
+@Controller
+public class SynchronizationController {
+
+	@RequestMapping(method=RequestMethod.GET, value= "/init")
+	public @ResponseBody String initialSynchronization(){
+		
+		return "success";
+	}
+	
+	@RequestMapping(value="/{lastSynchronizationDate}")
+	public @ResponseBody String  synchronization(@PathVariable Long lastSynchronizationDate){
+		
+		return "success";
+	}
+}
